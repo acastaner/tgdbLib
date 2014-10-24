528c8ce29a0f6933e9a3804ad3ac36d9675c6433
Arnaud Castaner <arnaud.castaner@outlook.com>
2014-10-24 13:32:05 +0200

    Added Platform and PlatformsData classes

---
+++ tgdbLib/Platform.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace tgdbLib
+{
+    public class Platform
+    {
+        [XmlElement("id")]
+        public int Id { get; set; }
+        [XmlElement("name")]
+        public string Name { get; set; }
+        [XmlElement("alias")]
+        public string Alias { get; set; }
+
+        /// <summary>
+        /// Returns a list of all the platforms.
+        /// </summary>
+        /// <returns></returns>
+        public static List<Platform> GetPlatformsList()
+        {
+            RequestHelper helper = new RequestHelper();
+            XmlDocument response = helper.MakeRequest("GetPlatformsList.php");
+            PlatformsData data = helper.Deserialize<PlatformsData>(response);
+            return data.Platforms;
+        }
+    }
+}
---
+++ tgdbLib/PlatformsData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace tgdbLib
+{
+    [XmlRoot("Data")]
+    public class PlatformsData
+    {
+        [XmlElement("basePlatformUrl")]
+        public string BaseUrl { get; set; }
+        public List<Platform> Platforms { get; set; }
+    }
+}
